1fd291d995bec39a8bbedd09e4705a9e840263e7
Jeffery Walsh <cyberhorsey@gmail.com>
2023-07-18 16:07:32 -0700

    Lfg

--- client/src/pages/ViewForAddress.svelte
+++ client/src/pages/ViewForAddress.svelte
@@ -25,9 +25,7 @@
 <div class="max-w-xl">
   <h1 class="mb-5 text-5xl font-bold">taikos on taiko</h1>
   {#if !$signer}
-    <p class="mb-5">
-      connect wallet first im not paying for rpc for u to search wallets lmao
-    </p>
+    <p class="mb-5">connect wallet first im not paying for rpc for u lmao</p>
     <ConnectWallet />
   {:else if nfts}
     <NftView {nfts} />
